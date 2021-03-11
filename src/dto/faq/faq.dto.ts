2665eeabb709a2812ef8bf648524e3a20d24c5b4
nate-nanuda <pjh@widaehan.com>
2021-03-11 12:29:27 +0900

    fix faq

--- src/dto/faq/faq.dto.ts
+++
@@ -1,6 +0,0 @@
-import { BaseDto } from '@/core';
-
-export class FaqDto extends BaseDto<FaqDto> {
-  faq: string;
-  answer: string;
-}
--- src/modules/faq/Faq.vue
+++ src/modules/faq/Faq.vue
@@ -5,44 +5,40 @@
       <div v-for="faq in faqList" :key="faq.id">
         <b-card class="m-2">
           <div class="m-3">Q: {{ faq.faq }}</div>
-          <div class="m-3">A: {{ faq.answer }}</div>
+          <div class="m-3" v-for="answer in faq.answers" :key="answer.id">
+            A: {{ answer.answer }}
+          </div>
         </b-card>
       </div>
     </template>
     <div v-else>
       <b-spinner label="Loading..."></b-spinner>
     </div>
-    <b-pagination
-      v-model="pagination.page"
-      :total-rows="listCount"
-      :per-page="pagination.limit"
-    ></b-pagination>
   </div>
 </template>
 <script lang="ts">
 import BaseComponent from '@/core/base.component';
-import { Component, Watch } from 'vue-property-decorator';
-import { Pagination } from '@/common';
+import { Component } from 'vue-property-decorator';
 import faqService from '@/services/faq.service';
+import { FaqRequestDto, FaqResponseDto } from '@/dto/faq';
 @Component({
   name: 'Faq',
 })
 export default class Faq extends BaseComponent {
-  private faqList: Faq[] = [];
-  private pagination = new Pagination(5);
-  private listCount: number = null;
-
-  @Watch('pagination', {
-    deep: true,
-  })
+  private faqList: FaqResponseDto[] = [];
+  private faqRequestDto = new FaqRequestDto();
   paginationChanged() {
     this.getList();
   }
 
   getList() {
-    faqService.findAll(this.pagination).subscribe(res => {
-      this.faqList = res.data.items;
-      this.listCount = res.data.totalCount;
+    faqService.findAll(this.faqRequestDto).subscribe(res => {
+      this.faqList = res.data;
+      this.faqList.forEach(e => {
+        faqService.findAll({ faqParentId: e.id }).subscribe(res => {
+          this.$set(e, 'answers', res.data);
+        });
+      });
     });
   }
   mounted() {
