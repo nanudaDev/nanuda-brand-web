ad95d855d0e9697a7981e7bcf9c1327c66c572c3
nate-nanuda <pjh@widaehan.com>
2021-03-17 18:07:00 +0900

    차트만드는 도중 커밋

---
+++ src/modules/_components/charts/ResultRevenueChart.vue
@@ -0,0 +1,89 @@
+<script lang="ts">
+// CommitChart.ts
+import BaseComponent from '@/core/base.component';
+import Vue from 'vue';
+import { Bar, Line, mixins } from 'vue-chartjs';
+import { Component } from 'vue-property-decorator';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+@Component({
+  extends: Line, // this is important to add the functionality to your component
+  mixins: [mixins.reactiveProp],
+})
+export default class ResultRevenueChart extends Vue {
+  public renderChart!: (chartData: any, options?: any) => void;
+  mounted(): void {
+    // Overwriting base render method with actual data.
+    this.renderChart(
+      {
+        labels: ['', '최저매출', '내 매출', '평균매출', '최고매출', ''],
+        datasets: [
+          {
+            backgroundColor: '#f87979',
+            data: [700, 900, 1200, 1400, 1600, 1800],
+            pointRadius: [0, 5, 20, 5, 5, 0],
+            pointBackgroundColor: [
+              '#f87979',
+              '#f87979',
+              'black',
+              '#f87979',
+              '#f87979',
+              '#f87979',
+            ],
+          },
+        ],
+      },
+      {
+        plugins: [ChartDataLabels],
+        layout: {
+          padding: {
+            // Any unspecified dimensions are assumed to be 0
+            top: 35,
+          },
+        },
+        tooltips: {
+          enabled: false,
+          showAllTooltips: true,
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false,
+        },
+        scales: {
+          xAxes: [
+            {
+              ticks: {
+                padding: -410,
+                fontSize: 20,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                fontColor: [
+                  'white',
+                  'white',
+                  'black',
+                  'white',
+                  'white',
+                  'white',
+                ],
+                min: 200,
+                max: 2000,
+                beginAtZero: true,
+                display: false,
+              },
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+        },
+      },
+    );
+  }
+}
+</script>
+
+<style></style>
--- src/modules/question/Solution.vue
+++ src/modules/question/Solution.vue
@@ -1,13 +1,19 @@
 <template>
-  <h3>Solution</h3>
+  <div>
+    <h3>Solution</h3>
+    <div>
+      <ResultRevenueChart />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import BaseComponent from '@/core/base.component';
 import { Component } from 'vue-property-decorator';
-
+import ResultRevenueChart from '@/modules/_components/charts/ResultRevenueChart.vue';
 @Component({
   name: 'Solution',
+  components: { ResultRevenueChart },
 })
 export default class Solution extends BaseComponent {}
 </script>
