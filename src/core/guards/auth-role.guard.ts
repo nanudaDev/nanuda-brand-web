3f382eb4bd45fa8633dc738a8c686f0915bb8da3
bbogeul <illumeweb@gmail.com>
2021-03-21 13:52:03 +0900

    added title guard for all pages

---
+++ src/core/guards/auth-role.guard.ts
@@ -0,0 +1,15 @@
+import { Route } from 'vue-router';
+import router from '@/router';
+
+const getPageTitle = (title: string) => {
+  const hasTitle = `${title}`;
+  if (hasTitle) {
+    return hasTitle;
+  }
+  return '픽쿡 - 실패 없는 창업 안내합니다';
+};
+
+router.afterEach((to: Route) => {
+  // set page title
+  document.title = getPageTitle(`${to.meta.title} - 픽쿡`);
+});
