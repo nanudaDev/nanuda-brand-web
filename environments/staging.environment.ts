622e950d50132b5ff37eac69ceead7083dfda55b
ria-nanuda <kbr@widaehan.com>
2021-05-20 14:51:21 +0900

    환경설정 baseURL 변경

--- environments/staging.environment.ts
+++ environments/staging.environment.ts
@@ -2,8 +2,8 @@ import packageInfo from '../package.json';
 import { Environment, EnvironmentType } from './environment.type';
 
 export const StagingEnvironment: Environment = {
-  // baseURL: 'https://platform-api-test.nanudakitchen.com/',
-  baseURL: 'https://pickcook-api.nanudakitchen.com/',
+  baseURL: 'https://platform-api-test.nanudakitchen.com/',
+  // baseURL: 'https://pickcook-api.nanudakitchen.com/',
   NODE_ENV: EnvironmentType.staging,
   production: false,
   clientName: packageInfo.name,
