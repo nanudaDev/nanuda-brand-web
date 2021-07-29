816fe06fb04abc23270f52b1f0bcbc2a68ed886f
ria-nanuda <kbr@widaehan.com>
2021-07-30 01:18:26 +0900

    샐러드 레시피 영상 버튼 기능 추가 & 디자인 CSS적용

--- src/modules/tutorials/JustSalad.vue
+++ src/modules/tutorials/JustSalad.vue
@@ -4,68 +4,97 @@
     :class="{ 'is-active': isVisibleChapter }"
     @keyup.stop.prevent="keyup($event)"
   >
-    <div class="video-wrapper">
+    <div class="video-wrapper" @click.stop.prevent="onPlayerClicked($event)">
+      <transition name="fadeIn">
+        <div class="video-title" v-if="!isPlaying">
+          <h1>저스트샐러드 레시피</h1>
+        </div>
+      </transition>
       <video-player
         class="video-player-box"
         ref="videoPlayer"
         :options="playerOptions"
         :playsinline="true"
         :controls="true"
+        @play="onPlayerPlay($event)"
+        @ended="onPlayerEnded($event)"
+        @pause="onPlayerPause($event)"
         @loadeddata="onPlayerLoadeddata($event)"
       >
       </video-player>
-    </div>
-    <div class="d-block d-lg-none">
-      <div class="chapter-title">
-        <div>
-          <h2>저스트 샐러드 레시피</h2>
-          <p>
-            수강 기한 : 무제한 <br />
-            <span>강의 수 : {{ chapterList.length }}강</span>
-            <span class="mx-2">|</span>
-            <span>시간 :{{ totalTime }}분</span>
-          </p>
-        </div>
+      <div class="btn-controller">
+        <transition name="fadeIn" mode="out-in">
+          <div class="btn-controll" v-if="!isStart">
+            <div class="btn-start"></div>
+          </div>
+        </transition>
+        <transition name="fadeIn" mode="out-in">
+          <div class="btn-controll" v-if="isEnded">
+            <div class="btn-restart"></div>
+          </div>
+        </transition>
+      </div>
+      <div class="btn-controller">
+        <transition name="fadeOutZoomIn">
+          <div class="btn-controll btn-start-pause" v-if="isVideoClicked">
+            <div class="btn-start" v-if="isPlaying"></div>
+            <div class="btn-pause" v-if="!isPlaying"></div>
+          </div>
+        </transition>
       </div>
     </div>
     <div class="chapter-wrapper">
-      <div class="d-block d-lg-none">
-        <div class="chapter-handle" @click="onToggleChanpter()">
-          <span class="handle-bar"></span>
-        </div>
-      </div>
-      <div class="d-none d-lg-block">
+      <div class="chapter-list-box">
         <div class="chapter-title">
           <div>
-            <h2>저스트 샐러드 레시피</h2>
-            <p>
-              수강 기한 : 무제한 <br />
-              <span>강의 수 : {{ chapterList.length }}강</span>
-              <span class="mx-2">|</span>
-              <span>시간 :{{ totalTime }}분</span>
-            </p>
+            <span class="en-title">JUST SALAD N JUICE</span>
+            <h2>레시피 목차</h2>
+          </div>
+        </div>
+        <div class="chapter-list">
+          <div
+            v-for="(chapterArr, index) in chapterList"
+            :key="index"
+            class="row-box"
+          >
+            <div
+              class="card rounded"
+              v-for="(chapter, childIndex) in chapterArr"
+              :key="childIndex"
+            >
+              <span class="sub-head yoongothic"
+                >Chapter {{ index > 8 ? index + 1 : `0${index + 1}` }}
+                {{ chapterArr.length > 1 ? `- ${childIndex + 1}` : null }}
+              </span>
+              <h3 class="headline">
+                {{ chapter.title }}
+              </h3>
+
+              <div v-if="chapter.trackList" class="track-container">
+                <div
+                  v-for="track in chapter.trackList"
+                  :key="track.time"
+                  class="track-list"
+                  @click="onMoveChapter(track.time)"
+                >
+                  <span class="track-title">{{ track.subject }}</span>
+                  <span class="track-time">
+                    <b-icon icon="play-circle-fill" class="icon"></b-icon>
+                    <span class="yoongothic txt-tiny ml-2">{{
+                      track.time | secondsToMinutesTransformer
+                    }}</span>
+                  </span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div class="chapter-list">
-        <div
-          v-for="(chapter, index) in chapterList"
-          :key="index"
-          class="border p-4 my-4 bg-light d-flex justify-content-between"
-          @click="onMoveChapter(chapter.time)"
-        >
-          <span class="ml-2">{{ chapter.title }}</span>
-          <b-badge variant="primary">{{
-            chapter.time | secondsToMinutesTransformer
-          }}</b-badge>
-        </div>
-      </div>
+      <span @click="onToggleChapter()" class="btn-toggle-chapter">
+        <span class="icon"><BaseArrow /></span>
+        <span class="is-blind"> {{ !isVisibleChapter ? '열기' : '닫기' }}</span>
+      </span>
     </div>
-    <span
-      @click="onToggleChanpter()"
-      class="btn btn-sm  btn-outline-white btn-toggle-chapter"
-      >{{ !isVisibleChapter ? '열기' : '닫기' }}</span
-    >
   </article>
 </template>
 <script lang="ts">
@@ -81,56 +110,105 @@ import { videoPlayer } from 'vue-video-player';
   },
 })
 export default class JustSalad extends BaseComponent {
+  private totalTime = 0;
+  private isStart = false;
+  private isEnded = false;
+  private isPlaying = false;
+  private isVideoClicked = false;
   private isVisibleChapter = true;
   private chapterList = [
-    {
-      title: '01 재료 새척',
-      time: 16,
-    },
-    {
-      title: '02 재료 손질',
-      time: 85,
-    },
-    {
-      title: '03 재료 조리',
-      time: 298,
-    },
-    {
-      title: '04 재료 담기',
-      time: 484,
-    },
-    {
-      title: '01 재료 새척',
-      time: 16,
-    },
-    {
-      title: '02 재료 손질',
-      time: 85,
-    },
-    {
-      title: '03 재료 조리',
-      time: 298,
-    },
-    {
-      title: '04 재료 담기',
-      time: 484,
-    },
-    {
-      title: '01 재료 새척',
-      time: 16,
-    },
-    {
-      title: '02 재료 손질',
-      time: 85,
-    },
-    {
-      title: '03 재료 조리',
-      time: 298,
-    },
-    {
-      title: '04 재료 담기',
-      time: 484,
-    },
+    [
+      {
+        title: '재료 새척',
+        trackList: [
+          {
+            subject: '재료 세척 방법',
+            time: 16,
+          },
+          {
+            subject: '채소 소독 방법',
+            time: 79,
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: '재료 손질',
+        trackList: [
+          {
+            subject: '재료 컷팅 및 손질 방법',
+            time: 84,
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: '재료 조리',
+        trackList: [
+          {
+            subject: '조리 방법(삶기, 볶기, 굽기, 튀기기)',
+            time: 298,
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: '재료 담기',
+        trackList: [
+          {
+            subject: '버섯 샐러드',
+            time: 483,
+          },
+          {
+            subject: '리코타 샐러드',
+            time: 538,
+          },
+          {
+            subject: '두부병아리콩 샐러드',
+            time: 565,
+          },
+          {
+            subject: '닭가슴살 샐러드',
+            time: 594,
+          },
+          {
+            subject: '콥 샐러드',
+            time: 614,
+          },
+          {
+            subject: '콩고기 샐러드',
+            time: 641,
+          },
+          {
+            subject: '구운 연어스테이크 샐러드 샐러드',
+            time: 665,
+          },
+        ],
+      },
+      {
+        title: '드레싱',
+        trackList: [
+          {
+            subject: '드레싱 만드는 방법 및 담기',
+            time: 683,
+          },
+        ],
+      },
+    ],
+    [
+      {
+        title: '배달 포장',
+        trackList: [
+          {
+            subject: '배달용 포장 방법',
+            time: 695,
+          },
+        ],
+      },
+    ],
   ];
 
   private playerOptions = {
@@ -155,14 +233,36 @@ export default class JustSalad extends BaseComponent {
     return (this.$refs.videoPlayer as any).player;
   }
 
-  private totalTime = 0;
   onPlayerLoadeddata(event?: EventTarget) {
     if (this.player.cache_) {
       this.totalTime = this.player.cache_.duration;
     }
   }
 
-  onToggleChanpter() {
+  onPlayerPlay(event?: EventTarget) {
+    this.isStart = true;
+    this.isPlaying = true;
+    this.isEnded = false;
+  }
+
+  onPlayerEnded(event?: EventTarget) {
+    this.isEnded = true;
+  }
+
+  onPlayerPause(event?: EventTarget) {
+    this.isPlaying = false;
+  }
+
+  onPlayerClicked(event?: EventTarget) {
+    if (this.isStart) {
+      this.isVideoClicked = true;
+      setTimeout(() => {
+        this.isVideoClicked = false;
+      }, 1000);
+    }
+  }
+
+  onToggleChapter() {
     this.isVisibleChapter = !this.isVisibleChapter;
   }
 
@@ -171,7 +271,7 @@ export default class JustSalad extends BaseComponent {
     this.player.play();
   }
 
-  keyup(key?: any) {
+  onKeyup(key?: any) {
     const vol = 0.1;
     const time = 10;
 
@@ -209,6 +309,7 @@ export default class JustSalad extends BaseComponent {
 
     // Press the space bar button
     if (key.keyCode === 32) {
+      this.onPlayerClicked();
       if (!this.player.paused()) {
         this.player.pause();
       } else {
@@ -217,12 +318,26 @@ export default class JustSalad extends BaseComponent {
     }
   }
 
+  handleDebouncedResizing() {
+    const screenWith = window.innerWidth;
+
+    if (screenWith <= 992) {
+      this.isVisibleChapter = true;
+    }
+  }
+
+  created() {
+    window.addEventListener('resize', this.handleDebouncedResizing);
+  }
+
   mounted() {
-    document.addEventListener('keyup', this.keyup);
+    this.handleDebouncedResizing();
+    document.addEventListener('keyup', this.onKeyup);
   }
 
   beforeDestroy() {
-    document.removeEventListener('keyup', this.keyup);
+    document.removeEventListener('keyup', this.onKeyup);
+    window.addEventListener('resize', this.handleDebouncedResizing);
   }
 }
 </script>
@@ -233,24 +348,91 @@ export default class JustSalad extends BaseComponent {
   overflow: hidden;
   flex-wrap: wrap;
   .video-wrapper {
+    position: relative;
     display: flex;
     align-items: center;
     width: 100%;
     height: 100vh;
     background-color: #000;
     transition: width 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+    .video-title {
+      position: absolute;
+      left: 0;
+      top: 0;
+      right: 0;
+      padding: 16px 16px;
+      color: #fff;
+      z-index: 2;
+      @include gradientBlack;
+      h1 {
+        font-size: 20px;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+      }
+    }
     .video-player {
       position: relative;
       width: 100%;
       height: inherit;
+      margin-top: 0;
       .video-js {
         position: absolute;
         width: 100% !important;
         height: 100% !important;
         .vjs-big-play-button {
+          display: none;
           left: 50%;
           top: 50%;
           transform: translate(-50%, -50%);
+        }
+      }
+    }
+    .btn-controller {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      width: 80px;
+      height: 80px;
+      transform: translate(-50%, -50%);
+      z-index: 2;
+      user-select: none;
+      pointer-events: none;
+      .btn-controll {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: rgba(0, 0, 0, 0.4);
+        font-size: 40px;
+        color: #fff;
+
+        &.btn-start-pause {
+          opacity: 0;
+        }
+
+        .btn-start {
+          &:before {
+            font-family: 'VideoJS';
+            content: '\f101';
+            vertical-align: middle;
+          }
+        }
+        .btn-pause {
+          &:before {
+            font-family: 'VideoJS';
+            content: '\f103';
+            vertical-align: middle;
+          }
+        }
+
+        .btn-restart {
+          &:before {
+            font-family: 'VideoJS';
+            content: '\f116';
+            vertical-align: middle;
+          }
         }
       }
     }
@@ -258,76 +440,113 @@ export default class JustSalad extends BaseComponent {
   .chapter-wrapper {
     position: relative;
     width: 0;
-    height: 100vh;
-    overflow: hidden;
-    z-index: 5;
     transition: width 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-    // transition: transform 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-    // transform: translate(100%, 0);
-
-    .chapter-handle {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      background-color: $primary;
-      border-bottom: 1px solid $border-color;
-      border-top-right-radius: 1em;
-      border-top-left-radius: 1em;
-      height: 3em;
-      .handle-bar {
-        display: block;
-        width: 4em;
-        height: 0.5em;
-        background-color: $white;
-        border-radius: 1em;
-      }
-    }
-    .chapter-title {
+    .chapter-list-box {
       position: relative;
-      z-index: 2;
-      display: flex;
-      justify-content: flex-start;
-      align-items: center;
-      height: 8em;
-      background-color: $primary;
-      color: $white;
-      padding: 1.25em;
-      box-shadow: $shadow;
-      h2 {
-        font-size: $txt-large;
-        font-weight: $txt-bold;
-      }
-      p {
-        font-size: $txt-small;
-        margin-top: 1em;
-      }
-    }
-    .chapter-list {
-      padding: 1.25em;
-      height: calc(100% - 8em);
-      background-color: #fff;
+      padding: 32px 24px;
       overflow-y: auto;
       -webkit-overflow-scrolling: touch;
+      height: 100vh;
+      z-index: 5;
+      background-color: #f5f5f5;
+      box-shadow: -0.25em 0 0.25em rgba(0, 0, 0, 0.05);
+
+      .chapter-title {
+        color: $black;
+        padding-left: 8px;
+        h2 {
+          font-size: 20px;
+          font-weight: 700;
+        }
+        .en-title {
+          font-size: 10px;
+          font-weight: 500;
+        }
+      }
+      .chapter-list {
+        margin-top: 24px;
+      }
+    }
+
+    .btn-toggle-chapter {
+      display: flex;
+      align-items: center;
+      position: absolute;
+      right: 100%;
+      top: 50%;
+      transform: translate(30px, -50%);
+      z-index: 4;
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+      background-color: #f5f5f5;
+      box-shadow: 0 0 0.25em rgba(0, 0, 0, 0.05);
+      padding-left: 8px;
+      .icon {
+        transform: rotate(90deg);
+        width: 16px;
+        fill: #8c8c8c;
+      }
     }
   }
-  .btn-toggle-chapter {
-    position: absolute;
-    right: 1.25em;
-    top: 1.25em;
-    z-index: 5;
+
+  .card {
+    padding: 20px 16px;
+    box-shadow: $shadow-sm;
+    .sub-head {
+      font-size: 12px;
+      font-weight: 700;
+      color: $black;
+    }
+    .headline {
+      font-size: 20px;
+      font-weight: 700;
+      color: $black;
+      margin-top: 4px;
+    }
+    .track-container {
+      margin-top: 8px;
+      .track-list {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 24px 0;
+        cursor: pointer;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        + .track-list {
+          border-top: 1px solid #e0e0e0;
+        }
+        &:last-child {
+          padding-bottom: 8px;
+        }
+        .track-title {
+          font-size: 16px;
+          font-weight: 400;
+        }
+        .track-time {
+          display: flex;
+          align-items: center;
+          font-size: 14px;
+          font-weight: 500;
+        }
+        &:hover,
+        &.is-active {
+          color: $blue;
+        }
+      }
+    }
   }
 
   &.is-active {
     .video-wrapper {
-      width: calc(100% - 500px);
+      width: calc(100% - 400px);
     }
     .chapter-wrapper {
-      width: 500px;
-      // transform: translate(0, 0);
-      .chapter-handle {
-        background-color: $white;
-        .handle-bar {
-          background-color: $border-color;
+      width: 400px;
+      .btn-toggle-chapter {
+        .icon {
+          transform: rotate(-90deg);
         }
       }
     }
@@ -339,17 +558,42 @@ export default class JustSalad extends BaseComponent {
     .video-wrapper {
       width: 100% !important;
       height: 54vw;
+      .video-title {
+        h1 {
+          font-size: 3vw;
+        }
+      }
     }
     .chapter-wrapper {
-      position: fixed;
       width: 100% !important;
-      bottom: 0;
-      height: 75vh;
-      box-shadow: 0 -0.5em 0.25em rgba(0, 0, 0, 0.05);
+      height: calc(100vh - 54vw);
       transition: transform 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
       transform: translate(0, calc(100% - 3em));
-      .chapter-list {
-        height: calc(100% - 3em);
+      .chapter-list-box {
+        padding: 24px 16px;
+        height: 100%;
+      }
+
+      .card {
+        .sub-head {
+          font-size: 10px;
+        }
+        .headline {
+          font-size: 16px;
+        }
+        .track-container {
+          .track-list {
+            &:last-child {
+              padding-bottom: 8px;
+            }
+            .track-title {
+              font-size: 14px;
+            }
+            .track-time {
+              font-size: 12px;
+            }
+          }
+        }
       }
     }
     &.is-active {
