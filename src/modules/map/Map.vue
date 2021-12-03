b977ec45e173081fb477fd101b4a482f81089bab
nate-nanuda <pjh@widaehan.com>
2021-12-03 11:06:37 +0900

    보이는 부분만 마커표시, 마커를 클릭하면 원생성

--- src/modules/map/Map.vue
+++ src/modules/map/Map.vue
@@ -15,7 +15,7 @@
       :marker="marker"
     >
       <div class="info-window-container" v-if="marker">
-        <h1>hello</h1>
+        <h1>{{ infoContent }}</h1>
       </div>
     </naver-info-window>
     <naver-marker
@@ -23,8 +23,15 @@
       v-for="(marker, i) in showingMarkers"
       :lat="marker.position.y"
       :lng="marker.position.x"
-      @click="onMarkerClicked"
+      @click="onMarkerClicked($event, marker)"
     />
+    <!-- <naver-circle
+      v-if="marker"
+      :lat="marker.position.y"
+      :lng="marker.position.x"
+      :radius="500"
+    /> -->
+    <naver-rectangle :south="36" :north="36.05" :west="126" :east="126.05" />
   </naver-maps>
 </template>
 
@@ -33,6 +40,7 @@ import BaseComponent from '@/core/base.component';
 import { Component } from 'vue-property-decorator';
 import Vue from 'vue/types/umd';
 import sosang from '@/assets/test-data/sosangoingin.json';
+import sangkwon from '@/assets/test-data/sangkwon.json';
 @Component({
   name: 'Map',
 })
@@ -40,16 +48,18 @@ export default class Map extends BaseComponent {
   private width = 1400;
   private height = 800;
   private info = false;
+  private infoContent = '';
   private marker: any = null;
   private count = 1;
   private map: any = null;
   private isCTT = false;
   private sosang = sosang.data;
   private showingMarkers = [];
+  private zoomLevel = 10;
   private mapOptions = {
     lat: 37,
     lng: 127,
-    zoom: 10,
+    zoom: this.zoomLevel,
     minZoom: 6,
     maxZoom: 20,
   };
@@ -61,14 +71,24 @@ export default class Map extends BaseComponent {
   // private heatmapCoords: any = [];
   private markers: any = [];
   private heatMap = null;
+  private sangkwon = sangkwon;
+  private circle = null;
+  private spotNearStores: any = [];
 
   updateMarkers() {
-    const mapBounds = this.map.getBounds();
-    this.showingMarkers = this.markers.filter(e => {
-      const position = e.getPosition();
-      // console.log('position', position);
-      if (mapBounds.hasLatLng(position)) return true;
-    });
+    console.log('updateMarkers');
+
+    if (this.zoomLevel > 14) {
+      const mapBounds = this.map.getBounds();
+      console.log('mapBounds', mapBounds);
+      this.showingMarkers = this.markers.filter(e => {
+        const position = e.getPosition();
+        // console.log('position', position);
+        if (mapBounds.hasLatLng(position)) return true;
+      });
+    } else {
+      this.showingMarkers = [];
+    }
   }
   startHeatMap() {
     this.heatMap = new window.naver.maps.visualization.HeatMap({
@@ -77,41 +97,94 @@ export default class Map extends BaseComponent {
       colorMap: window.naver.maps.visualization.SpectrumStyle.HOT,
     });
   }
+  removeNearStores() {
+    this.spotNearStores.forEach(e => {
+      e.setMap(null);
+    });
+    this.spotNearStores = [];
+  }
 
   onLoad(vue: any) {
     console.log('naver', window.naver);
 
     this.map = vue;
-    this.sosang.forEach(e => {
-      const position = new window.naver.maps.LatLng(e[5], e[4]);
+    this.sangkwon.forEach(e => {
+      const position = new window.naver.maps.LatLng(
+        e.spotLatitude,
+        e.spotLongitude,
+      );
+      if (e.storeCount > 4) {
+        const marker = new window.naver.maps.Marker({
+          map: this.map,
+          position: position,
+          title: e.spotId,
+          zIndex: 100,
+        });
+        this.markers.push(marker);
+      }
 
-      const marker = new window.naver.maps.Marker({
-        map: this.map,
-        position: position,
-        zIndex: 100,
-      });
-      this.markers.push(marker);
       // this.heatmapCoords.push(position);
     });
+    console.log('sangkwon', sangkwon);
+
+    this.updateMarkers();
   }
   onWindowLoad(that: any) {
     console.log('that', that);
   }
-  onMarkerClicked(event: any) {
+  onMarkerClicked(event: any, marker: any) {
+    console.log('event', event);
+    console.log('marker', marker);
     this.info = false;
     this.$nextTick(() => {
       this.info = true;
     });
-
+    this.removeNearStores();
     this.marker = event.overlay;
+    const spot = sangkwon.find(e => e.spotId == marker.title);
+
+    this.infoContent = `${spot.spotId}`;
+
+    // this.circle.setMap(this.marker.position);
+    if (this.circle) this.circle.setMap(null);
+    this.circle = new window.naver.maps.Circle({
+      map: this.map.map,
+      radius: 500,
+      center: this.marker.position,
+      fillColor: 'navy',
+      fillOpacity: 0.1,
+    });
+
+    if (spot.store.length > 0) {
+      spot.store.forEach(e => {
+        const marker = new window.naver.maps.Marker({
+          map: this.map,
+          position: new window.naver.maps.LatLng(
+            e.storeLatitude,
+            e.storeLongitude,
+          ),
+          title: e.storeId,
+          zIndex: 100,
+        });
+        this.spotNearStores.push(marker);
+      });
+    }
+    console.log('spotNearStores', this.spotNearStores);
+
+    this.spotNearStores.forEach(e => {
+      e.setMap(this.map.map);
+    });
   }
   onZoomChanged(zoomLevel: number) {
     console.log('on zoom changed vue', zoomLevel);
+    this.zoomLevel = zoomLevel;
     this.updateMarkers();
   }
   onDrag() {
     this.updateMarkers();
+    this.removeNearStores();
     this.info = false;
+    if (this.circle) this.circle.setMap(null);
   }
   onInitStyleMap() {
     this.startHeatMap();
